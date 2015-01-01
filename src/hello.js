bffd300d9746d247402208add988443ee44dfd44
Dan Shappir <dansh@wix.com>
2015-01-01 15:21:48 +0200

    code cleanup

--- src/hello.js
+++ src/hello.js
@@ -1,11 +1,8 @@
 define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
-    var Playground = React.createClass({
-        displayName: 'Hello',
-        render: function () {
-            return template.apply(this);
-        }
-    });
 
-    return Playground;
+    return React.createClass({
+        displayName: 'Hello',
+        render: template
+    });
 });
